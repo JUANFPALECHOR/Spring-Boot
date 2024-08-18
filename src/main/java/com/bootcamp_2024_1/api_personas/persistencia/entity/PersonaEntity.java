469ce2ddda96c15e7f95150b273a262e533213e9
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-08-18 16:06:35 -0500

    Descripción de los cambios realizados

--- src/main/java/com/bootcamp_2024_1/api_personas/persistencia/entity/PersonaEntity.java
+++ src/main/java/com/bootcamp_2024_1/api_personas/persistencia/entity/PersonaEntity.java
@@ -9,6 +9,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/*
+
+    la funcion de esta clase es convertir la clase a una tabla en la base de datos
+
+    @Entity = Marca la clase PersonaEntity como una entidad JPA. Esto significa que esta clase se mapeará a una tabla en la base de datos.
+     Cada instancia de esta clase representará una fila en la tabla.
+
+    @Data = biblioteca que se usa para evitar escribir código repetitivo, como getters, setters, constructores,
+     y otros métodos comunes en las clases.
+
+    IDENTITY, le estás diciendo a JPA que deje que la base de datos se encargue de generar el valor de id automáticamente
+
+
+
+
+ */
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -19,7 +36,66 @@ public class PersonaEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nombre;
     private String apellido;
+
+    //ESTO LO HACE AUTOMATICAMENTE @NoArgsConstructor
+
+    // Constructor sin argumentos (obligatorio para JPA)
+    //public PersonaEntity() {
+    //}
+
+    //ESTO LO HACE AUTOMATICAMENTE @AllArgsConstructor
+
+    // Constructor con todos los campos (opcional para facilitar la creación de objetos)
+    //public PersonaEntity(Long id, String nombre, String apellido) {
+    //    this.id = id;
+    //    this.nombre = nombre;
+    //    this.apellido = apellido;
+    //}
+
+
+    // ESTO LO HACE AUT @DATA
+
+    /*
+
+        // Getters y Setters para todos los campos
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+
+
+
+
+
+
+
+
+     */
+
+
+
+
+
 }
--- src/main/java/com/bootcamp_2024_1/api_personas/persistencia/repository/PersonaRepository.java
+++ src/main/java/com/bootcamp_2024_1/api_personas/persistencia/repository/PersonaRepository.java
@@ -5,7 +5,26 @@ import com.bootcamp_2024_1.api_personas.persistencia.entity.PersonaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository//indica que esta clase se encargará de las operaciones con la base de datos.
+
+
 
 public interface PersonaRepository extends JpaRepository<PersonaEntity, Long> {
 }
+
+
+
+/*
+
+    Al extender JpaRepository, obtienes acceso a un conjunto de métodos predefinidos para trabajar con tu entidad PersonaEntity.
+     Estos métodos se implementan automáticamente y no necesitas escribir el código para ellos. Algunos de los métodos más útiles incluyen:
+
+    save(T entity): Guarda (inserta o actualiza) una entidad en la base de datos.
+    findById(ID id): Busca una entidad por su clave primaria (id).
+    findAll(): Devuelve una lista de todas las entidades en la tabla correspondiente.
+    deleteById(ID id): Elimina una entidad por su clave primaria.
+    existsById(ID id): Verifica si existe una entidad con el id dado.
+    count(): Devuelve el número de entidades en la tabla.
+
+
+ */
